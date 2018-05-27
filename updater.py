fadb0db3af2a26a867aea8026ab3240eb9da59a2
cdsw <c.dswibowo@yahoo.com.tw>
2018-05-27 21:50:56 +0900

    UPDATER DONE

--- updater.py
+++ updater.py
@@ -1,36 +1,48 @@
 import zipfile
 import os, distutils.dir_util
+import shutil
 
 class Updater:
-    def __init__(self, mainApp, path):
-        #self.mainAppPath = ...........
-        self.mainApp = mainApp
-        #self.ver = mainApp.ver
+    def __init__(self, path):
+        version_file = open("version.v","r")
+        self.ver = int(version_file.read())
+        version_file.close()
         self.path = path
         self.updateable = False
-
-    def closeMainApp(self):
-        pass        
         
     def validateApp(self):
-        zip_ref = zipfile.ZipFile(self.path, 'r')
-        zip_ref.extractall('./update')
-        zip_ref.close()
-        #check if current version is latest already'
-        for d, s, fn in os.walk("./ver"):
-            if fname < self.ver:
+        try:
+            print(self.path)
+            zip_ref = zipfile.ZipFile(self.path, 'r')
+            zip_ref.extractall('./update')
+            zip_ref.close()
+            #check if current version is latest already'
+            version_file = open("./update/version.v","r")
+            updater_file_ver = int(version_file.read())
+            version_file.close()
+            if updater_file_ver > self.ver:
                 self.updateable = True
-        #check update validity
-
+                return 1
+            else:
+                print("Linguis is already in the latest version")
+                return 0                
+        except:
+            print("Invalid updater package.")
+            return -1
+        
     def updateApp(self):
-        distutils.dir_util.copy_tree(self.path, self.mainAppPath)
-        #remove empty files
-        for dirName, subdirList, fileList in os.walk(self.mainAppPath):
-            for fname in fileList:
-                if os.stat(dirName + "\\" + fname).st_size == 0 && fname[-2:0]:
-                    os.remove(dirName + "\\" + fname)
-        #delete updater cache
-        os.remove(self.path)
-
-    def relaunchApp(self):
-        pass
+        if self.updateable == True:
+            distutils.dir_util.copy_tree("./update", "./")
+            #remove empty files
+            for dirName, subdirList, fileList in os.walk("./"):
+                for fname in fileList:
+                    if os.stat(dirName + "\\" + fname).st_size == 0 and fname[-2:0]:
+                        os.remove(dirName + "\\" + fname)
+            #delete updater cache
+            shutil.rmtree("./update")
+            return True
+        return False
+        
+def main():
+    u = Updater("E:\\Studium\\Universitat\\2018-1\\SEP\\s9\\version.zip")
+    u.updateApp()
--- updaterUI.py
+++ updaterUI.py
@@ -1,7 +1,9 @@
 import sys, os
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 from updater_UI import Ui_MainWindow as Uiu
+import updater as updater_
 
 class UpdaterUI(QMainWindow):
     def __init__(self):
@@ -10,21 +12,42 @@ class UpdaterUI(QMainWindow):
         self.ui.setupUi(self)
         self.ui.searchButton.clicked.connect(self.getUpdateFileName)
         self.updaterLoc = None
+        self.updater = None
+        self.ui.updateButton.clicked.connect(self.updateFirmware)
 
     def getUpdateFileName(self):
         try:
-            fn = QFileDialog.getOpenFileName(self, 'Open file', os.getcwd(),"Rar(*.rar)")
+            fn = QFileDialog.getOpenFileName(self, 'Open file', os.getcwd(),"Zip file (*.zip)")
             self.updaterLoc = fn[0]
         except:
             print("No file selected.")
-        print(self.updaterLoc)
         if self.updaterLoc != None:
-            self.pathField.setText(self.updaterLoc)
+            self.ui.pathField.setText(self.updaterLoc)
+            self.updater = updater_.Updater(self.updaterLoc)
+            self.ui.pathField.setText(self.updaterLoc)
 
+    def updateFirmware(self):
+        self.updaterLoc = self.ui.pathField.text()
+        if self.updaterLoc != None:
+            self.updater = updater_.Updater(self.updaterLoc)
+            stat = self.updater.validateApp()
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Information)
+            msg.setStandardButtons(QMessageBox.Ok)
+            if stat == 1:
+                self.updater.updateApp()
+                msg.setText("Linguis is updated to version "+ str(self.))
+                msg.setWindowTitle("Updated.")
+            elif stat == 0:
+                msg.setText("Linguis is already in the latest version.")
+                msg.setWindowTitle("Not updated.")
+            else:
+                msg.setText("Invalid installer package")
+                msg.setWindowTitle("Not updated.")
+            msg.exec_()
+                
 def main():
     app = QApplication(sys.argv)
     u = UpdaterUI()
     u.show()
     return app.exec_()
-
-main()
